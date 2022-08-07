1325c7c447a6b5afd5391bc1db03854fc52d4dab
not82 <vendrouxf@gmail.com>
2022-08-07 17:08:55 +0200

    Added client basic management

---
+++ database/migrations/2022_08_07_150609_client_email.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class ClientEmail extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('client', function (Blueprint $table) {
+            $table->string('email');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
